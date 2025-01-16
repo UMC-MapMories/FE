9012d34d2746062b586f057a7f7676aef2592d2c
박성준 <sungjunp1208@gmail.com>
2025-01-16 15:23:22 +0900

    feat/3: BottomNavigationView 초안

---
+++ app/src/main/java/com/devdi/mapmories/DiaryFragment.kt
@@ -0,0 +1,17 @@
+package com.devdi.mapmories
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class DiaryFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_diary, container, false)
+    }
+}
--- app/src/main/java/com/devdi/mapmories/MainActivity.kt
+++ app/src/main/java/com/devdi/mapmories/MainActivity.kt
@@ -1,20 +1,42 @@
 package com.devdi.mapmories
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.devdi.mapmories.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // 앱 시작 시 HomeFragment로 시작
+        replaceFragment(HomeFragment())
+
+        binding.bottomNavigationView.selectedItemId = R.id.home
+
+        // BottomNavigationView 메뉴 클릭 리스너
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.home -> replaceFragment(HomeFragment())
+                R.id.diary -> replaceFragment(DiaryFragment())
+                R.id.people -> replaceFragment(PeopleFragment())
+                R.id.shop -> replaceFragment(ShopFragment())
+                R.id.settings -> replaceFragment(SettingsFragment())
+                else -> {}
+            }
+            true
         }
+    }
+
+    // 프래그먼트 전환 메서드
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.frame_layout, fragment)
+            .commit()
     }
 }
