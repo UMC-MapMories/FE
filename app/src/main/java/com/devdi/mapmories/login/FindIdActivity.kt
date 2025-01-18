01362eb5c3994378bc91315524b6955ad9c3bcbd
박성준 <sungjunp1208@gmail.com>
2025-01-18 17:06:56 +0900

    feat/5: LoginActivity 추가

---
+++ app/src/main/java/com/devdi/mapmories/login/FindIdActivity.kt
@@ -0,0 +1,15 @@
+package com.devdi.mapmories.login
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.devdi.mapmories.R
+
+class FindIdActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_find_id)
+    }
+}
---
+++ app/src/main/java/com/devdi/mapmories/login/InputNumberActivity.kt
@@ -0,0 +1,15 @@
+package com.devdi.mapmories.login
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.devdi.mapmories.R
+
+class InputNumberActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_input_number)
+    }
+}
