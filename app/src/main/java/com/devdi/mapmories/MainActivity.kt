e8890ff8e49c3be4148f77a37d7f4b1531ead874
박성준 <sungjunp1208@gmail.com>
2025-02-02 17:08:15 +0900

    feat/23: SettingsFragment, ShopFragment 화면 UI 구현

--- app/src/main/java/com/devdi/mapmories/MainActivity.kt
+++ app/src/main/java/com/devdi/mapmories/MainActivity.kt
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.devdi.mapmories.databinding.ActivityMainBinding
 import com.devdi.mapmories.people.PeopleFragment
+import com.devdi.mapmories.settings.SettingsFragment
+import com.devdi.mapmories.shop.ShopFragment
 
 class MainActivity : AppCompatActivity() {
 
